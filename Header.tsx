6245def0090f771b6629675a652a1003f24a7822
talim053 <talinkhan053@gmail.com>
2025-06-18 20:02:16 +0530

    Create Header.tsx

---
+++ Header.tsx
@@ -0,0 +1,12 @@
+// components/Header.tsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Office Supply Tracker</h1>
+    </header>
+  );
+};
+
+export default Header;
